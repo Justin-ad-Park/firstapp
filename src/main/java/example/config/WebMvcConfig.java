6c65fe73ecec811829d28a387751621bba33976f
justin <justin.ad.park@gmail.com>
2018-11-28 23:42:04 +0900

    How to change XML configuration and Java configuration

--- src/main/java/example/config/WebMvcConfig.java
+++ src/main/java/example/config/WebMvcConfig.java
@@ -12,7 +12,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
+    public void configureViewResolvers(ViewResolverRegistry registry)
+    {
         registry.jsp();
     }
 }
