1c3b0df7f1fabf03617b0869951c07741564c1b1
cjloveu <justin@11street.co.th>
2018-12-10 19:16:52 +0900

    Test AOP v0.1

--- src/main/java/example/app/WebcomeController.java
+++ src/main/java/example/app/WebcomeController.java
@@ -2,6 +2,10 @@ package example.app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class WebcomeController {
@@ -9,5 +13,16 @@ public class WebcomeController {
     @RequestMapping("/")
     public String home() {
         return "index";
+    }
+
+
+    @RequestMapping("/aoptest")
+    public ModelAndView home(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+
+        response.setHeader("Cache-Control", "no-cache");
+
+        ModelAndView mav = new ModelAndView("echo/aop");
+        mav.addObject("aopData", "AOP Test");
+        return mav;
     }
 }
---
+++ src/main/webapp/WEB-INF/echo/aop.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: cjloveu
+  Date: 10/12/2018
+  Time: 7:04 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>AOP Test</title>
+</head>
+<body>
+    AOP Test
+</body>
+</html>
