cd30842058856b1c2be318e04c5bcae334feceec
cjloveu <justin@11street.co.th>
2018-12-10 15:06:50 +0900

    JSONArray sample

---
+++ src/test/java/test/TestJSONArray.java
@@ -0,0 +1,25 @@
+package test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.junit.Test;
+
+@Slf4j
+public class TestJSONArray {
+
+    /**
+     *  Add Pom.xml
+     *     <dependency>
+     *       <groupId>com.vaadin.external.google</groupId>
+     *       <artifactId>android-json</artifactId>
+     *       <version>0.0.20131108.vaadin1</version>
+     *     </dependency>
+     */
+//    @Test
+//    public void testJSONArray() {
+//        JSONArray jsonArray = new JSONArray();
+//
+//        log.debug("JSON : " + jsonArray.toString());
+//
+//    }
+}
