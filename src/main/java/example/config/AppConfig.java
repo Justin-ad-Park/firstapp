6ee62ca74d65ad718cb3fc719558770cca4ab2e0
cjloveu <justin@11street.co.th>
2018-12-11 14:17:13 +0900

    remove unused imports

--- src/main/java/example/config/AppConfig.java
+++ src/main/java/example/config/AppConfig.java
@@ -1,8 +1,6 @@
 package example.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 public class AppConfig {
